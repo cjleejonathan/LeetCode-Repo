f23431556ab93559dccb827fcd2243fbe91230b4
Jonathan Lee <88358193+cjleejonathan@users.noreply.github.com>
2021-11-29 14:10:29 -0800

    Update 721.java

--- 721.java
+++ 721.java
@@ -1,34 +1,86 @@
 class Solution {
-    public List<List<String>> accountsMerge(List<List<String>> acts) {
-        Map<String, String> owner = new HashMap<>();
-        Map<String, String> parents = new HashMap<>();
-        Map<String, TreeSet<String>> unions = new HashMap<>();
-        for (List<String> a : acts) {
-            for (int i = 1; i < a.size(); i++) {
-                parents.put(a.get(i), a.get(i));
-                owner.put(a.get(i), a.get(0));
+    /**
+     * Solution 1: Union Find
+     *
+     * Use two hash map with union find class to solve the problem
+     * @params mailToIndex: one to one mapping: mail string to its parent index mapping
+     * @params disjointSet: one to many mapping: parent index to all emails that belong to same group mapping
+     * */
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        if (accounts.size() == 0) {
+            return new ArrayList<>();
+        }
+
+        int n = accounts.size();
+        UnionFind uf = new UnionFind(n);
+
+        // Step 1: traverse all emails except names, if we have not seen an email before, put it with its index into map.
+        // Otherwise, union the email to its parent index.
+        Map<String, Integer> mailToIndex = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < accounts.get(i).size(); j++) {
+                String curMail = accounts.get(i).get(j);
+                if (mailToIndex.containsKey(curMail)) {
+                    int preIndex = mailToIndex.get(curMail);
+                    uf.union(preIndex, i);
+                }
+                else {
+                    mailToIndex.put(curMail, i);
+                }
             }
         }
-        for (List<String> a : acts) {
-            String p = find(a.get(1), parents);
-            for (int i = 2; i < a.size(); i++)
-                parents.put(find(a.get(i), parents), p);
+
+        // Step 2: traverse every email list, find the parent of current list index and put all emails into the set list
+        // that belongs to key of its parent index
+        Map<Integer, Set<String>> disjointSet = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            // find parent index of current list index in parent array
+            int parentIndex = uf.find(i);
+            disjointSet.putIfAbsent(parentIndex, new HashSet<>());
+
+            Set<String> curSet = disjointSet.get(parentIndex);
+            for (int j = 1; j < accounts.get(i).size(); j++) {
+                curSet.add(accounts.get(i).get(j));
+            }
+            disjointSet.put(parentIndex, curSet);
         }
-        for(List<String> a : acts) {
-            String p = find(a.get(1), parents);
-            if (!unions.containsKey(p)) unions.put(p, new TreeSet<>());
-            for (int i = 1; i < a.size(); i++)
-                unions.get(p).add(a.get(i));
+
+        // step 3: traverse ket set of disjoint set group, retrieve all emails from each parent index, and then SORT
+        // them, as well as adding the name at index 0 of every sublist
+        List<List<String>> result = new ArrayList<>();
+        for (int index : disjointSet.keySet()) {
+            List<String> curList = new ArrayList<>();
+            if (disjointSet.containsKey(index)) {
+                curList.addAll(disjointSet.get(index));
+            }
+            Collections.sort(curList);
+            curList.add(0, accounts.get(index).get(0));
+            result.add(curList);
         }
-        List<List<String>> res = new ArrayList<>();
-        for (String p : unions.keySet()) {
-            List<String> emails = new ArrayList(unions.get(p));
-            emails.add(0, owner.get(p));
-            res.add(emails);
-        }
-        return res;
+        return result;
     }
-    private String find(String s, Map<String, String> p) {
-        return p.get(s) == s ? s : find(p.get(s), p);
+
+    class UnionFind {
+        int size;
+        int[] parent;
+        public UnionFind(int size) {
+            this.size = size;
+            this.parent = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public void union(int a, int b) {
+            parent[find(a)] = parent[find(b)];
+        }
+
+        public int find(int x) {
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
     }
 }
